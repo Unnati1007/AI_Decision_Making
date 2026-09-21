97acdd0c7082f0b01b72ea966576ce5dbea21c9e
Unnati1007 <unnatijadon1007@gmail.com>
2026-09-22 01:25:58 +0530

    Include frontend/src/lib/utils.js for build resolution

---
+++ frontend/src/lib/utils.js
@@ -0,0 +1,9 @@
+import { clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
+
+/**
+ * Merges tailwind classes intelligently.
+ */
+export function cn(...inputs) {
+  return twMerge(clsx(inputs));
+}
